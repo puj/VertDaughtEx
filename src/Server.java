0ce94cf72d8cb06aaf9c5c5dc8f44e03ac985471
Anders Jansson <anders@izettle.com>
2012-09-14 01:10:31 +0200

    Added response type json to /users.

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,20 +9,19 @@ import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.deploy.Verticle;
 
-
 public class Server extends Verticle {
-	
+
 	private static List<Object> users = new ArrayList<>();
-	
+
 	public void start() {
 		HttpServer server = vertx.createHttpServer();
 		RouteMatcher routeMatcher = new RouteMatcher();
 		setRouts(routeMatcher);
 		server.requestHandler(routeMatcher).listen(8008);
 	}
-	
+
 	private void setRouts(RouteMatcher routeMatcher) {
-		routeMatcher.get("/", new Handler<HttpServerRequest>(){
+		routeMatcher.get("/", new Handler<HttpServerRequest>() {
 
 			@Override
 			public void handle(HttpServerRequest req) {
@@ -31,59 +29,61 @@ public class Server extends Verticle {
 			}
 		});
 
-		routeMatcher.get("/enter", new Handler<HttpServerRequest>(){
+		routeMatcher.get("/enter", new Handler<HttpServerRequest>() {
 
 			@Override
 			public void handle(HttpServerRequest req) {
 				users.add(req.params().get("nick"));
 				sendRedirect(req, "/chat");
 			}
-			
+
 		});
-		
-		routeMatcher.get("/chat", new Handler<HttpServerRequest>(){
+
+		routeMatcher.get("/chat", new Handler<HttpServerRequest>() {
 
 			@Override
 			public void handle(HttpServerRequest req) {
 				req.response.sendFile("chat.html");
 			}
-			
+
 		});
-		
-		routeMatcher.get("/users", new Handler<HttpServerRequest>(){
+
+		routeMatcher.get("/users", new Handler<HttpServerRequest>() {
+			
 			@Override
 			public void handle(HttpServerRequest req) {
 				JsonObject object = new JsonObject();
 				JsonArray json = new JsonArray(users);
 				object.putArray("users", json);
+				req.response.headers().put("Content-Type","application/json");
 				req.response.end(object.encode());
-				
+
 			}
-			
+
 		});
-		
-		routeMatcher.get("/js/:filename", new Handler<HttpServerRequest>(){
-			
+
+		routeMatcher.get("/js/:filename", new Handler<HttpServerRequest>() {
+
 			@Override
 			public void handle(HttpServerRequest req) {
 				req.response.sendFile("js/" + req.params().get("filename"));
 			}
-			
+
 		});
-		
-routeMatcher.get("/css/:filename", new Handler<HttpServerRequest>(){
-			
+
+		routeMatcher.get("/css/:filename", new Handler<HttpServerRequest>() {
+
 			@Override
 			public void handle(HttpServerRequest req) {
 				req.response.sendFile("css/" + req.params().get("filename"));
 			}
-			
+
 		});
 	}
-	private void sendRedirect(HttpServerRequest req, String url) {    
-	    req.response.putHeader("Location", url);
-	    req.response.statusCode = 302;
-	    req.response.end();
-	  }
-}
 
+	private void sendRedirect(HttpServerRequest req, String url) {
+		req.response.putHeader("Location", url);
+		req.response.statusCode = 302;
+		req.response.end();
+	}
+}
