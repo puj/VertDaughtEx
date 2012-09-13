cb6dc9a065e1db133e0f4e5899d155e5c4f82c8f
Anders Jansson <anders@izettle.com>
2012-09-13 22:30:47 +0200

    fix 404

--- org/hack/UrlHandler.java
+++ org/hack/UrlHandler.java
@@ -2,14 +2,22 @@ public class UrlHandler {
 
 	public static String resolve(String url) {
 		if(url == null){
+			System.out.println("im null");
 			throw new IllegalArgumentException();
 		}
 
 		switch(url){
-		case "/":
+		case "":
+			System.out.println("im the empty string");
 			return "index.html";
+		case "/":
+			System.out.println("im the slash");
+			return "index.html";
+		case "/favicon.ico":
+			System.out.println("im the favicon");
+			return "";
 		default:
-			throw new IllegalArgumentException();
+			return "404.html";
 		}
 	}
 }
