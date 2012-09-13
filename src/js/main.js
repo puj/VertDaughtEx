83e2ccdbcfac4c1f8e3f1334272428efa45ff29b
puj <vango85@gmail.com>
2012-09-13 23:21:58 +0100

    Polling chat box sending messages concat(^^^^^^^^^^^^^^^^,shit);

---
+++ src/js/main.js
@@ -0,0 +1,61 @@
+
+function init(){
+	$("#inputBox").focus();
+	$("#inputBox").width($("#chatWindow").width());
+	setInterval(pollServer,500);	
+}
+	
+
+function pollServer(){
+	$.getJSON({
+		url:"/users",
+	}).complete(showUsers);
+}
+
+function sendMessage(){
+	var inputBox = $("#inputBox");
+	if(inputBox.val().length == 0){
+		return;
+	}
+	var inputData = inputBox.val();
+	inputBox.val("");
+
+	$.ajax({
+	  type: 'POST',
+	  url: "/message",
+	  data: inputData,
+	  dataType: "json"
+	});
+
+	inputBox.empty();
+}
+
+function setTitle(){
+	$("#chatWindow").append("OMG AHWE TERMS  C-HAT ~!! !L!!!<br>");
+	$("#chatWindow").append("=========================================<br>");
+}
+
+function showUsers(data){
+	var chatWindow = $("#chatWindow");
+	chatWindow.empty();
+	setTitle();
+
+	if(!data || !data.users || data.users.length == 0){
+		chatWindow.append("No users connected...");
+		return;
+	}
+
+	var userList = data.users;
+	var userListString = "";
+	for(var user in userList){
+		userListString += (user + (user == userList[userList.length-1]?"":", "));
+	}
+	chatWindow.append(userList.length + " user(s) connected : "  + userListString);
+}
+
+function handleKeyPress(e){
+	var key=e.keyCode || e.which;
+	if (key==13){
+		sendMessage();
+	}
+}
